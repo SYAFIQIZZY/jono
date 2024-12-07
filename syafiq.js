74b4717c3b0dbe14864b3231c19e7bb6982a1185
SYAFIQIZZY <syafiqizzy953@gmail.com>
2024-12-08 02:19:46 +0700

    Add files via upload

---
+++ syafiq.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Pribadi</title>
+</head>
+<body>
+    <h1>Portfolio Pribadi</h1>
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Person",
+      "name": "Muhammad Syafiq Izzy",
+      "identifier": {
+        "@type": "PropertyValue",
+        "name": "NIM",
+        "value": "123456789"
+      }
+      "memberOf": {
+        "@type": "EducationalOrganization",
+        "name": "Nama Universitas",
+        "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "Alamat Kampus",
+          "addressLocality": "Kota",
+          "addressRegion": "Provinsi",
+          "postalCode": "Kode Pos",
+          "addressCountry": "Negara"
+        },
+        "telephone": "+62-123-456-7890",
+        "openingHours": "Mo-Fr 08:00-17:00",
+        "department": {
+          "@type": "EducationalOrganization",
+          "name": "Nama Kelas",
+          "course": {
+            "@type": "Course",
+            "name": "Mata Kuliah"
+          }
+        }
+      }
+    }
+    </script>
+</body>
+</html>
+
